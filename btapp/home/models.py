0ac2fbbb2a68331acf9a36d62322b2f714a92966
Manprime <187654232+Manprime@users.noreply.github.com>
2024-11-12 16:11:26 +0000

    Added 1st basic setup
    Added models, forms, authetication for siginin , signout , register , display income and expenses etc.

---
+++ btapp/home/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Profile(models.Model):
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Income(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    source=models.CharField(max_length=50)
+    date = models.DateField()
+    
+    def __str__(self):
+        return self.user
+
+class Expense(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    exp_name=models.CharField(max_length=50)
+    category = models.CharField(max_length=50)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    date = models.DateField()
+    
+    def __str__(self):
+        return self.user
+    
+class Goals(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    goal_name=models.CharField(max_length=50)
+    g_amount=models.DecimalField(max_digits=10, decimal_places=2)
+    g_category=models.CharField(max_length=50)
+    g_creatioDate = models.DateField()
+    g_deadline=models.DateField()
+    
+    def __str__(self):
+        return self.user
+
