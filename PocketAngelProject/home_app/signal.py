c54e13086bd384626f20e3c4c7d416be0d1c0d4b
Manprime <187654232+Manprime@users.noreply.github.com>
2025-03-01 12:28:39 +0000

    Delete PocketAngelProject directory

--- PocketAngelProject/home_app/signal.py
+++
@@ -1,9 +0,0 @@
-from django.dispatch import Signal
-from django.dispatch import receiver
-from .models import Alert
-
-budget_alert_signal = Signal()
-
-@receiver(budget_alert_signal)
-def create_alert(sender, user, message, **kwargs):
-    Alert.objects.create(user=user, message=message)
